2b395598f110185b99231f4b53d0475862bb1299
whiskas009 <76867818+whiskas009@users.noreply.github.com>
2022-05-12 20:41:47 +0300

    #1

---
+++ Task_6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Сколько скристалов вы хотите купить ?");
+            int cristal = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Сколько у Вас золота ?");
+            int gold = int.Parse(Console.ReadLine());
+
+            int priceForCristal = 13;
+            gold -= priceForCristal * cristal;
+
+            Console.WriteLine($"Вы купили {cristal} кристалов и у вас осталось {gold} золота");
+        }
+    }
+}
